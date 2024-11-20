43faf0d55f00f26a3de8c22c64ac28baacbcc139
MohammedYaseenkalvium <m.yaseensayed09@gmail.com>
2024-11-20 10:49:22 +0530

    inheritance

--- ColonySim.cpp
+++ ColonySim.cpp
@@ -4,26 +4,15 @@
 using namespace std;
 
 class Colony {
-private:
+protected:
     string name;
     int population;
-    static int total_colonies;
 
 public:
-    Colony(string n = "Unknown", int pop = 0) : name(n), population(pop) {
-        total_colonies++;
-    }
+    Colony(string n = "Unknown", int pop = 0) : name(n), population(pop) {}
 
-    static int getTotalColonies() {
-        return total_colonies;
-    }
-
-    string getName() const {
-        return name;
-    }
-
-    void setName(string n) {
-        name = n;
+    virtual void report() const {
+        cout << "Colony Name: " << name << ", Population: " << population << endl;
     }
 
     int getPopulation() const {
@@ -33,82 +22,70 @@ public:
     void setPopulation(int pop) {
         population = pop;
     }
+};
 
-    void growPopulation(int amount) {
-        population += amount;
-        cout << "The population of " << name << " has grown by " << amount << ". New population: " << population << endl;
-    }
 
-    void reportPopulation() const {
-        cout << "The current population of " << name << " is " << population << "." << endl;
+class SpaceColony : public Colony {
+private:
+    string planetName;
+
+public:
+    SpaceColony(string n, int pop, string planet)
+        : Colony(n, pop), planetName(planet) {}
+
+    void report() const override {
+        cout << "Space Colony Name: " << name
+             << ", Planet: " << planetName
+             << ", Population: " << population << endl;
     }
 };
 
-int Colony::total_colonies = 0;
-
 class Alien {
-private:
+protected:
     string speciesName;
     int strength;
-    static int total_attacks;
 
 public:
-    Alien(string sName = "Unknown", int str = 0) : speciesName(sName), strength(str) {}
+    Alien(string sName = "Unknown", int str = 0)
+        : speciesName(sName), strength(str) {}
 
-    static int getTotalAttacks() {
-        return total_attacks;
-    }
-
-    string getSpeciesName() const {
-        return speciesName;
-    }
-
-    void setSpeciesName(string sName) {
-        speciesName = sName;
-    }
-
-    int getStrength() const {
-        return strength;
-    }
-
-    void setStrength(int str) {
-        strength = str;
-    }
-
-    void attack(Colony &colony) {
-        total_attacks++;
+    virtual void attack(Colony &colony) {
         cout << "The " << speciesName << " attacks the colony!" << endl;
         if (strength > colony.getPopulation()) {
-            cout << "The " << speciesName << " is too strong! The colony is overrun." << endl;
+            cout << "The colony is overrun by " << speciesName << "." << endl;
         } else {
-            cout << "The colony defends itself against the " << speciesName << "." << endl;
+            cout << "The colony defends itself successfully!" << endl;
         }
     }
 };
 
-int Alien::total_attacks = 0;
+class HostileAlien : public Alien {
+private:
+    int aggressionLevel;
+
+public:
+    HostileAlien(string sName, int str, int aggression)
+        : Alien(sName, str), aggressionLevel(aggression) {}
+
+    void attack(Colony &colony) override {
+        cout << "The hostile alien " << speciesName
+             << " with aggression level " << aggressionLevel
+             << " attacks!" << endl;
+        Alien::attack(colony);
+    }
+};
 
 int main() {
-    Colony* colonies = new Colony[3]{
-        Colony("Earth Colony", 100),
-        Colony("Mars Colony", 150),
-        Colony("Jupiter Colony", 80)
-    };
+    SpaceColony earth("Earth Colony", 1000, "Earth");
+    SpaceColony mars("Mars Colony", 500, "Mars");
 
-    Alien* alienSpecies = new Alien[2]{
-        Alien("Xenomorph", 120),
-        Alien("Predator", 200)
-    };
+    HostileAlien xenomorph("Xenomorph", 800, 9);
 
-    colonies[0].growPopulation(50);
-    alienSpecies[0].attack(colonies[0]);
-    alienSpecies[1].attack(colonies[2]);
+    earth.report();
+    xenomorph.attack(earth);
 
-    cout << "Total colonies created: " << Colony::getTotalColonies() << endl;
-    cout << "Total alien attacks: " << Alien::getTotalAttacks() << endl;
-
-    delete[] colonies;
-    delete[] alienSpecies;
+    mars.report();
+    xenomorph.attack(mars);
 
     return 0;
 }
